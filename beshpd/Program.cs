257156c3feb6bde29a6345265c2c2c95d83880ef
Besh Prakash Yogi <beshprakashktm@gmail.com>
2025-10-23 18:32:32 +0200

    project setup updated

--- beshpd/Program.cs
+++ beshpd/Program.cs
@@ -1,18 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+app.MapFallbackToController("Index", "Fallback");
 
 app.Run();
